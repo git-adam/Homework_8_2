0055698942ef62adea5dcb1cfa62ddf8666e08e8
git-adam <awwojcik1@gmail.com>
2024-03-04 19:32:35 +0100

    add logger

---
+++ SmsService/SmsService/SmsService.cs
@@ -0,0 +1,33 @@
+﻿using NLog.Fluent;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmsService
+{
+    public partial class SmsService : ServiceBase
+    {
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
+        public SmsService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Logger.Info("Sms Service started ...");
+        }
+
+        protected override void OnStop()
+        {
+            Logger.Info("Sms Service stopped ...");
+        }
+    }
+}
